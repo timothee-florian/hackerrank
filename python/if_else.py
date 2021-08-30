153faedaff09881af499b456dff81d86f4061f36
timothee-florian <timothee.florian.bronner@gmail.com>
2021-08-31 00:09:24 +0200

    trivial if elif algo that say if a number is weird

---
+++ python/if_else.py
@@ -0,0 +1,19 @@
+import math
+import os
+import random
+import re
+import sys
+
+def is_weird(n):
+    if n % 2:
+        print('Weird')
+    elif n <= 5:
+        print('Not Weird')
+    elif n <= 20:
+        print('Weird')
+    else:
+        print('Not Weird')
+
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+    is_weird(n)
