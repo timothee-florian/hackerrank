ef142773eb984c5e22005a27151a8d66508fcbc5
timothee-florian <timothee.florian.bronner@gmail.com>
2021-09-15 01:05:07 +0200

    list creation and reducing

--- basics.scala
+++ basics.scala
@@ -3,3 +3,23 @@
 def f(delim:Int,arr:List[Int]):List[Int] ={
     return arr.filter(_ < delim)
 }
+
+// https://www.hackerrank.com/challenges/fp-array-of-n-elements/problem
+// Create list of n elements, had to define readInt
+object Solution extends App {
+
+    def f(num:Int) : List[Int] = {
+        val list = List.range(0, num)
+
+        return list
+    }
+
+    val readInt = scala.io.StdIn.readInt
+    println(f(readInt))
+}
+
+// https://www.hackerrank.com/challenges/fp-sum-of-odd-elements/problem
+// sum odd numbers
+def f(arr:List[Int]):Int = {
+    return arr.filter(x => x % 2 == 1).reduce((x, y) => x + y)
+}
