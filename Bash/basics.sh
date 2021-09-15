a8e08db2b7c32cd3c5148293c2e8d78f9eb722bf
timothee-florian <timothee.florian.bronner@gmail.com>
2021-09-15 22:35:17 +0200

    arithmetic and if else statement

--- Bash/basics.sh
+++ Bash/basics.sh
@@ -1,7 +1,6 @@
 # https://www.hackerrank.com/challenges/bash-tutorials-lets-echo/problem
 # print 
 echo HELLO
-########################################################################
 
 # https://www.hackerrank.com/challenges/bash-tutorials---a-personalized-echo/problem
 # read input
@@ -14,5 +13,47 @@ i=1
 while [ $i -le 50 ]
 do
     echo $i
+    i=$((i+1))
+done
+
+
+# https://www.hackerrank.com/challenges/bash-tutorials---the-world-of-numbers/problem
+# Arithmetic
+read X
+read Y
+echo "$((X + Y))"
+echo "$((X - Y))"
+echo "$((X * Y))"
+echo "$((X / Y))"
+echo "$((X % Y))"
+
+
+# https://www.hackerrank.com/challenges/bash-tutorials---more-on-conditionals/problem
+# if elif else
+read X
+read Y
+read Z
+
+if [[ $X == $Y && $Y == $Z ]]
+then
+    echo 'EQUILATERAL';
+elif [[ $X == $Y || $Y == $Z ]]
+then
+    echo 'ISOSCELES';
+else
+    echo 'SCALENE';
+fi
+
+# https://www.hackerrank.com/challenges/bash-tutorials---looping-and-skipping/problem
+# loop if skip
+i=1
+while [ $i -le 99 ]
+do
+    let "modu = $i % 2"
+    if [ $modu == 1 ]
+    then
+    echo $i
+    fi
+    
     i=$((i+1))
 done
