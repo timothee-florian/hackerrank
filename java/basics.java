a1c56ed5b2bceee9433ad60dcf8c20c252358cec
timothee-florian <timothee.florian.bronner@gmail.com>
2021-09-16 14:35:59 +0200

    get 1st start

--- java/basics.java
+++ java/basics.java
@@ -16,3 +16,60 @@ public class Solution {
         System.out.println(c);
     }
 }
+
+
+// https://www.hackerrank.com/challenges/java-if-else/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+// if else
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if (N % 2 == 1) {
+            System.out.println("Weird");
+        }
+        else if (N <= 5) {
+            System.out.println("Not Weird");
+        }
+        else if (N <= 20) {
+            System.out.println("Weird");
+        }
+        else {
+            System.out.println("Not Weird");
+        }
+
+        scanner.close();
+    }
+}
+
+
+// https://www.hackerrank.com/challenges/java-stdin-stdout/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+// stdIn stdout
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();  // empty the rest of the line
+        String s =  scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
